b08b75822331e0683645e321acdc88323178288e
James Doran <jysdoran@gmail.com>
2015-07-30 14:59:25 +0100

    Attempt 2

---
+++ Assets/Scripts/previewBrain.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class previewBrain : MonoBehaviour {
+
+	public int type ;
+	// Use this for initialization
+	/*void Start () {
+		gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite> ("Unit" + (type+1).ToString ());
+	}*/
+	
+	// Update is called once per frame
+}
