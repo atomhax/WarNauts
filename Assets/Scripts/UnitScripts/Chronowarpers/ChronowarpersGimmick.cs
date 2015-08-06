e6fa352969af61e6883cf46a4eaecd641c24c0da
James Doran <jysdoran@gmail.com>
2015-08-06 21:04:31 +0800

    Stuff left out from last commit
    Whoops

---
+++ Assets/Scripts/UnitScripts/Chronowarpers/ChronowarpersGimmick.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChronowarpersGimmick : MonoBehaviour {
+
+	short teleCount;
+	UnitBrain unitBrain;
+
+	// Use this for initialization
+	void Start () {
+		teleCount = 1;
+
+		unitBrain = GetComponent<UnitBrain> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (unitBrain.currentHealth < unitBrain.maxHealth/5 && teleCount > 0) {
+			if (Random.value<0.3) {
+				if (unitBrain.enemy!=null) {
+					if (unitBrain.axis%2==0) {
+						transform.position = new Vector3 (unitBrain.enemy.transform.position.x+(0.3f*unitBrain.moveDirection),transform.position.y,transform.position.z) ;
+					} else {
+						transform.position = new Vector3 (transform.position.x,unitBrain.enemy.transform.position.y+(0.3f*unitBrain.moveDirection),transform.position.z) ;
+					}
+				}
+			}
+			teleCount-- ;
+		}
+	}
+}
